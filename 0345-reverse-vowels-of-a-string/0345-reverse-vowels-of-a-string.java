13338c4ca1ce2023e3d10d0266212b46de66340a
Devesh Rathod <deveshrathod12@gmail.com>
2026-07-09 01:30:22 +0530

    Time: 2 ms (99.27%), Space: 46.5 MB (65.32%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -3,7 +3,7 @@ class Solution {
         int start = 0;
         int end = s.length()-1;
         char[] arr = s.toCharArray();
-        while(start <= end){
+        while(start < end){
             if(arr[start] == 'A'|| arr[start] == 'E'||arr[start] == 'I'||arr[start] == 'O'||arr[start] == 'U'||arr[start] == 'a'||arr[start] == 'e'||arr[start] == 'i'||arr[start] == 'o'||arr[start] == 'u'){
                 if((arr[end] == 'A'|| arr[end] == 'E'||arr[end] == 'I'||arr[end] == 'O'||arr[end] == 'U'||arr[end] == 'a'||arr[end] == 'e'||arr[end] == 'i'||arr[end] == 'o'||arr[end] == 'u')){
                     char temp = arr[start];
